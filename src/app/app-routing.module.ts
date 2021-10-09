57754674818bce8a65ec4db42da1c0f8ab37c42e
marcelohenrique95 <marcelo_95_@hotmail.com>
2021-10-09 14:39:04 -0400

    criando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { PageErrorComponent } from './component/page-error/page-error.component';
+import { ObrasComponent } from './screen/obra/obras.component';
+import { HomeComponent } from './screen/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'obra',
+    component: ObrasComponent
+  },
+  {
+    path: '**',
+    component: PageErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
