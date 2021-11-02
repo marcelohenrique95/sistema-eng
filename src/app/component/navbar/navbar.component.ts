57115ea171315ec4c68810091246289dff8556a0
Marcelo Henrique <77021035+marcelohenrique95@users.noreply.github.com>
2021-11-02 13:28:08 -0300

    adiçao de relogio de sessao online

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public interval: any;
+  public time: number;
+  public minutes: number;
+  public seconds: number;
 
   public menu = [
     {label:'INÍCIO', icon:'fas fa-home', router:'/home'},
@@ -18,9 +22,41 @@ export class NavbarComponent implements OnInit {
     {label:'SAIR', icon:'fas fa-sign-out-alt', router:'/login'},
   ]
 
-  constructor() { }
+  constructor() {
+    this.interval;
+    this.time = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+   }
 
   ngOnInit() {
+    this.startClock()
   }
 
+  public startClock(): void{
+    this.interval = setInterval(() => {
+      if(this.time === 0) {
+        this.time++;
+        this.seconds++;
+      } else {
+        this.time++;
+        this.seconds++;
+        if(this.seconds > 60) {
+          this.seconds = 0;
+        }
+      }
+      this.transformMinutes(this.time);
+    }, 1000)
+  }
+
+  public transformMinutes(value: number): string {
+    this.minutes = Math.floor(value / 60);
+    return this.minutes.toString();
+  }
+
+  public resetClock(): void {
+    clearInterval(this.interval);
+    this.time = 0;
+    this.seconds = 0;
+  }
 }
